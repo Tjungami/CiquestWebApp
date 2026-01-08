8a916ac40edec08da31f0de17495ac6722e4bffe
Tjungami <utm2477043@stu.o-hara.ac.jp>
2026-01-08 15:04:02 +0900

    update

--- ciquest_model/apps.py
+++ ciquest_model/apps.py
@@ -1,22 +1,6 @@
-from django.contrib import admin
-from .models import (
-    Rank, User, StoreOwner, Store, Coupon, Challenge,
-    UserChallenge, UserCoupon, StoreStampSetting, StoreStampReward,
-    StoreStamp, Tag, StoreTag, AdminAccount, AdminInquiry
-)
+from django.apps import AppConfig
 
-admin.site.register(Rank)
-admin.site.register(User)
-admin.site.register(StoreOwner)
-admin.site.register(Store)
-admin.site.register(Coupon)
-admin.site.register(Challenge)
-admin.site.register(UserChallenge)
-admin.site.register(UserCoupon)
-admin.site.register(StoreStampSetting)
-admin.site.register(StoreStampReward)
-admin.site.register(StoreStamp)
-admin.site.register(Tag)
-admin.site.register(StoreTag)
-admin.site.register(AdminAccount)
-admin.site.register(AdminInquiry)
+
+class CiquestModelConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "ciquest_model"
