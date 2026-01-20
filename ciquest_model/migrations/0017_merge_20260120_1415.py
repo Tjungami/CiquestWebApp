a392b56197ce6e7c04a3c8776c81161d0faf39e1
Tjungami <utm2477043@stu.o-hara.ac.jp>
2026-01-20 14:47:00 +0900

    Update

---
+++ ciquest_model/migrations/0017_merge_20260120_1415.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("ciquest_model", "0016_alter_store_latitude_longitude"),
+        ("ciquest_model", "0004_coupon_usage_history"),
+    ]
+
+    operations = []
